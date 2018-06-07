939719ae5d3e2ac6ec1b27edb23ffd11dadc34cd
Maxime Nadeau <maxime.nadeau@outlook.com>
2018-06-07 14:21:16 -0400

    Image is now saved in attachment

---
+++ KillChain/model/diagrams.rb
@@ -0,0 +1,54 @@
+require 'data_mapper'
+require 'dm-migrations'
+
+# /plugins/KillChain/db/diagrams.db
+
+# Initialize the Master DB
+DataMapper.setup(:diagrams, "sqlite://#{Dir.pwd}/plugins/KillChain/db/diagrams.db")
+
+class Diagram
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :report_id, Integer, :required => false
+    property :attachment_id, Integer, :required => false
+
+end
+
+class Element
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :name, String, :required => true, :length => 500
+    property :key_name, String, :required => true, :length => 25
+    property :is_start, Boolean, :required => true, :default => false
+    property :class_id, Integer, :required => false
+    property :diagram_id, Integer, :required => true
+
+end
+
+class Relationship
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :from_id, Integer, :required => true
+    property :to_key, String, :required => false
+
+end
+
+class ClassDefinitions
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :diagram_id, Integer, :required => true
+    property :name, String, :required => true, :length => 50
+    property :css, String, :required => true, :length => 500
+
+end
+
+DataMapper.repository(:diagrams) {
+  Diagram.auto_migrate!
+  Element.auto_migrate!
+  Relationship.auto_migrate!
+  ClassDefinitions.auto_migrate!
+}
--- KillChain/routes.rb
+++ KillChain/routes.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'rmagick'
-require './plugins/UDV_Worksheet/master_udv'
+require './plugins/KillChain/model/diagrams'
 
 get '/KillChain/public/:file' do
 	send_file('plugins/KillChain/public/'+params[:file], :disposition => 'inline')
@@ -14,7 +14,48 @@ get '/KillChain' do
   report = get_report(report_id)
   return 'No Such Report' if report.nil?
 
+	@report_id = report_id
+
 	@findings = Findings.all(report_id: report_id)
+
+	DataMapper.repository(:diagrams) {
+		@diagram = Diagram.first(report_id: report_id)
+
+		if !@diagram
+			@diagram = Diagram.new()
+			@diagram.report_id = report_id
+		  @diagram.save
+
+			classDef1 = ClassDefinitions.new()
+			classDef1.diagram_id = @diagram.id
+			classDef1.name = "asset"
+			classDef1.css = "fill:#008000,stroke-width:0px";
+		  classDef1.save
+
+			classDef2 = ClassDefinitions.new()
+			classDef2.diagram_id = @diagram.id
+			classDef2.name = "attack"
+			classDef2.css = "fill:#FF0000,stroke-width:0px";
+		  classDef2.save
+
+			@classDefs = Array.new
+			@classDefs.push(classDef1)
+			@classDefs.push(classDef2)
+
+			startElement = Element.new()
+			startElement.diagram_id = @diagram.id
+			startElement.name = "Start"
+			startElement.key_name = "id1"
+			startElement.is_start = true
+		  startElement.save
+
+			@graphElements = Array.new
+			@graphElements.push(startElement)
+		else
+			@classDefs = ClassDefinitions.all(diagram_id: @diagram.id)
+			@graphElements = Element.all(diagram_id: @diagram.id)
+		end
+	}
 
 	haml :"../plugins/KillChain/views/chart", :encode_html => true
 end
@@ -23,16 +64,55 @@ post '/KillChain' do
 	redirect to("/") unless valid_session?
 
 	data = request.POST
+
+	report_id = data['report_id']
+
+  # Query for the first report matching the id
+  report = get_report(report_id)
+  return 'No Such Report' if report.nil?
+
 	svg_string = data['svgText']
 
 	img = Magick::Image.from_blob(svg_string) {
 	  self.format = 'SVG'
 	  self.background_color = 'transparent'
 	}
-	File.open('/tmp/test.png', 'wb') do |f|
-	  f.write img[0].to_blob {
-							self.format = 'PNG'
-						}
+
+	DataMapper.repository(:diagrams) {
+		@diagram = Diagram.first(report_id: report_id)
+	}
+
+	@attachment = Attachments.first(id: @diagram.attachment_id)
+
+	if @attachment
+		File.open(@attachment.filename_location, 'wb') do |f|
+		  f.write img[0].to_blob {
+								self.format = 'PNG'
+							}
+		end
+	else
+		rand_file = "./attachments/#{rand(36**36).to_s(36)}.png"
+
+		File.open(rand_file, 'wb') do |f|
+		  f.write img[0].to_blob {
+								self.format = 'PNG'
+							}
+		end
+
+		# delete the file data from the attachment
+		datax = {}
+		# to prevent traversal we hardcode this
+		datax['filename_location'] = rand_file.to_s
+		datax['filename'] = 'KillChain_Plugin_Diagram'
+		datax['description'] = 'KillChain_Plugin_Diagram'
+		datax['report_id'] = report_id
+		data = url_escape_hash(datax)
+
+		@attachment = Attachments.new(data)
+		@attachment.save
+
+		@diagram.attachment_id = @attachment.id
+		@diagram.save
 	end
 
 	redirect ("/KillChain")
